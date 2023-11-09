f26edd202d2a020469a097001835e09bc3340f54
gray <137192656+grayhax@users.noreply.github.com>
2023-11-08 21:15:10 -0500

    Update settings.py

--- closedverse/settings.py
+++ closedverse/settings.py
@@ -27,7 +27,7 @@ PROD = False
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = ''
 
-ALLOWED_HOSTS = ['99.59.38.155', '192.168.1.208'
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost'
 ]
 
 
